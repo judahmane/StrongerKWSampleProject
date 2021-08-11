5aa278be8c9c5a75738395c3ad043bc7203df8a2
Judah <67433376+judahmane@users.noreply.github.com>
2021-08-11 18:32:40 -0400

    Add files via upload

---
+++ Stronger.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <Windows.h>
+#include <mmsystem.h>
+
+int main()
+{
+	std::string input;
+	std::cout << "Would You Like To Player Strong By Kanye West With Lyrics?" << std::endl;
+	std::cin >> input;
+	if (input == "Yes" || input == "yes")
+	{
+		PlaySound(TEXT(".wav Destination"), NULL, SND_FILENAME | SND_ASYNC); //This Might disrupt the rest up the code. I forgot How to fix it, But its from deleting NULL, SND_FILENAME, or SND_ASYNC Define in the parameters or whatever they are called. Idk Im not a pro yet.
+		Sleep(1000);
+		std::cout << "Work it, make it, do it Makes us harder, better, faster, stronger " << std::endl;
+		Sleep(4800);
+		std::cout << "N-now th-that that don't kill me" << std::endl;
+		Sleep(2500);
+		std::cout << "Can only make me stronger" << std::endl;
+		Sleep(2500);
+		std::cout << "I need you to hurry up now" << std::endl;
+		Sleep(2300);
+		std::cout << "'Cause I can't wait much longer" << std::endl;
+		Sleep(2500);
+		std::cout << "I know I got to be right now" << std::endl;
+		Sleep(2300);
+		std::cout << "'Cause I can't get much wronger" << std::endl;
+		Sleep(2300);
+		std::cout << "Man I've been waiting all night now" << std::endl;
+		Sleep(2300);
+		std::cout << "That's how long I been on ya" << std::endl;
+		Sleep(4500);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+		Sleep(100);
+		std::cout << "" << std::endl;
+
+
+	}
+
+
+
+}
